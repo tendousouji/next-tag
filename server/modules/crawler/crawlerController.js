bf7ab683a7e6615987ee947b8e5e7bb6e7b6eaa6
tendousouji <tendoumt1994@gmail.com>
2017-03-20 01:22:09 +0700

    Finish function iterate all links in homepage & get the sub from links array

--- server/modules/crawler/crawlerController.js
+++ server/modules/crawler/crawlerController.js
@@ -28,86 +28,80 @@ function crawl(url, word, baseUrl, callback) {
 
       var $ = cheerio.load(body);
 
-      // if(searchForWord($, word)) {
-      //   console.log('Word ' + word + ' found at page: ' + url);
-      // } else {
+      if(searchForWord($, word)) {
+        console.log('Word ' + word + ' found at page: ' + url);
+        return callback(null, pages);
+      } else {
+        var absoluteLinks = $("a[href^='http']");
+        var relativeLinks = $("a[href^='/']");
 
-      // }
-
-      var productArray = $('.product-item   ');
-      console.log(productArray.length);
-      for (var k = 1; k <= productArray.length; ++k) {
-        // console.log($('.product-item:nth-child(' + k + ')').parent().attr('class'));
-        if($('.product-item:nth-child(' + k + ')').parent().attr('class') == 'product-box-list') {
-          console.log('----------------------------------------------------------------------');
-          console.log($('.product-item:nth-child(' + k + ')').children().attr('href'));
-          console.log($('.product-item:nth-child(' + k + ')').children().attr('title'));
-          console.log($('.product-item:nth-child(' + k + ')').children().children('.price-sale').text());
-
-          var length = $('.product-item:nth-child(' + k + ')').children().children('.image').children('img').length;
-          if(length > 1) {
-            console.log($('.product-item:nth-child(' + k + ')').children().children('.image').children('img:nth-child(2)').attr('src'));
-          } else {
-            console.log($('.product-item:nth-child(' + k + ')').children().children('.image').children('img').attr('src'));
-          }
-
-          console.log($('.product-item:nth-child(' + k + ')').children().children('.review').text());
-        }
-      }
-
-      // console.log('----------------------------------------------------------------------');
-      // console.log($('.product-item   ').first().children().attr('href'));
-      // console.log($('.product-item   ').first().children().children('.title').text());
-      // console.log($('.product-item   ').first().children().children('.price-sale').text());
-
-      // if($('.product-item   ').first().children().children('.image').children('img').hasClass('lazy')) {
-      //   console.log($('.product-item   ').first().children().children('.image').children('img').attr('data-src'));
-      // } else {
-      //   console.log($('.product-item   ').first().children().children('.image').children('img').attr('src'));
-      // }
-
-      // console.log($('.product-item   ').first().children().children('.review').text());
-      
-      var absoluteLinks = $("a[href^='http']");
-      var relativeLinks = $("a[href^='/']");
-
-      for (var i = 0; i < absoluteLinks.length; ++i) {
-        var link = absoluteLinks[i].attribs.href;
-        if(pages.indexOf(link) == -1) {
-          if(link.includes(baseUrl)) {
-            pages.push(link);
+        for (var i = 0; i < absoluteLinks.length; ++i) {
+          var link = absoluteLinks[i].attribs.href;
+          if(pages.indexOf(link) == -1) {
+            if(link.includes(baseUrl)) {
+              pages.push(link);
+            }
           }
         }
-      }
 
-      for (var j = 0; j < relativeLinks.length; ++j) {
-        var link = relativeLinks[j].attribs.href;
-        var fullLink = baseUrl+link;
-        if(pages.indexOf(fullLink) == -1) {
-          pages.push(fullLink);
+        for (var j = 0; j < relativeLinks.length; ++j) {
+          var link = relativeLinks[j].attribs.href;
+          var fullLink = baseUrl+link;
+          if(pages.indexOf(fullLink) == -1) {
+            pages.push(fullLink);
+          }
         }
+
+        return callback(null, pages);
       }
 
+      // var productArray = $('.product-item   ');
+      // console.log(productArray.length);
+      // for (var k = 1; k <= productArray.length; ++k) {
+      //   if($('.product-item:nth-child(' + k + ')').parent().attr('class') == 'product-box-list') {
+      //     console.log('----------------------------------------------------------------------');
+      //     console.log($('.product-item:nth-child(' + k + ')').children().attr('href'));
+      //     console.log($('.product-item:nth-child(' + k + ')').children().attr('title'));
+      //     console.log($('.product-item:nth-child(' + k + ')').children().children('.price-sale').text());
+
+      //     var length = $('.product-item:nth-child(' + k + ')').children().children('.image').children('img').length;
+      //     if(length > 1) {
+      //       console.log($('.product-item:nth-child(' + k + ')').children().children('.image').children('img:nth-child(2)').attr('src'));
+      //     } else {
+      //       console.log($('.product-item:nth-child(' + k + ')').children().children('.image').children('img').attr('src'));
+      //     }
+
+      //     console.log($('.product-item:nth-child(' + k + ')').children().children('.review').text());
+      //   }
+      // }
+
+      // var absoluteLinks = $("a[href^='http']");
+      // var relativeLinks = $("a[href^='/']");
+
+      // for (var i = 0; i < absoluteLinks.length; ++i) {
+      //   var link = absoluteLinks[i].attribs.href;
+      //   if(pages.indexOf(link) == -1) {
+      //     if(link.includes(baseUrl)) {
+      //       pages.push(link);
+      //     }
+      //   }
+      // }
+
+      // for (var j = 0; j < relativeLinks.length; ++j) {
+      //   var link = relativeLinks[j].attribs.href;
+      //   var fullLink = baseUrl+link;
+      //   if(pages.indexOf(fullLink) == -1) {
+      //     pages.push(fullLink);
+      //   }
+      // }
+
+      // return callback(null, pages);
+
+    } else {
       return callback(null, pages);
-
     }
   });
 }
-
-// function collectInternalLinks($) {
-//   var absoluteLinks = $("a[href^='http']");
-//   var relativeLinks = $("a[href^='/']");
-  
-//   absoluteLinks.each(() => {
-//     pagesToVist.push($(this).attr('href'));
-//   });
-
-//   relativeLinks.each(() => {
-//     pagesToVist.push(baseUrl + $(this).attr('href'));
-//   });
-
-//   console.log(pagesToVist.length);
-// }
 
 class CrawlerController {
   constructor(kernel) {
@@ -139,28 +133,144 @@ class CrawlerController {
       console.log(pages.length);
 
       if(pages.length > 0) {
-        // var count = 0;
-        // var temps = pages.slice(0, 1);
-        var temps = ['https://tiki.vn/pin-sac-may-anh/c2662'];
-        // var limit = Math.floor(pages.length/8);
-        // console.log(limit);
+        var temps = pages.slice(0, 10);
 
-        async.mapLimit(temps, 10, (link, callback) => {
-          // ++count;
-          console.log(link);
-          crawl(link, searchWord, baseUrl, (error, newPages) => {
-            if(error) {
-              return callback(null, []);
-            } else {
-              // console.log(count);
-              return callback(null, newPages.length);
-            }
-          });
-        }, (err, response) => {
+        console.log(Math.floor(pages.length/5));
+        var splitArray = []
+        var limit = Math.floor(pages.length/5);
+        var start = 0;
+        var end = limit;
+
+        for (var t = 0; t < 5; ++t) {
+          var temps = pages.slice(start, end);
+          if(temps.length > 0) {
+            splitArray.push(temps);
+          } else {
+            splitArray.push([]);
+          }
+          start = end;
+          end += limit;
+        }
+
+        // for (var t = 0; t < 5; ++t) {
+        //   console.log(splitArray[t].length);
+        // }
+
+        // var temps = ['https://tiki.vn/pin-sac-may-anh/c2662'];
+        async.waterfall([
+          (cb) => {
+            let resutlArray = []
+            async.mapLimit(splitArray[0], 10, (link, callback) => {
+              crawl(link, searchWord, baseUrl, (error, newPages) => {
+                if(error) {
+                  return callback(null, []);
+                } else {
+                  return callback(null, newPages);
+                }
+              });
+            }, (err, resp) => {
+              if(err) { return console.log(err); }
+              // console.log(resp);
+              for (var t = 0; t < resp.length; ++t) {
+                for (var l = 0; l < resp[t].length; ++l) {
+                  if(resutlArray.indexOf(resp[t][l]) == -1) {
+                    resutlArray.push(resp[t][l]);
+                  }
+                }
+              }
+              return cb(null, resutlArray);
+            });
+          },
+          (arg1, cb) => {
+            let resutlArray = arg1;
+            async.mapLimit(splitArray[1], 10, (link, callback) => {
+              crawl(link, searchWord, baseUrl, (error, newPages) => {
+                if(error) {
+                  return callback(null, []);
+                } else {
+                  return callback(null, newPages);
+                }
+              });
+            }, (err, resp) => {
+              if(err) { return console.log(err); }
+              for (var t = 0; t < resp.length; ++t) {
+                if(resutlArray.indexOf(resp[t]) == -1) {
+                  resutlArray.push(resp[t]);
+                }
+              }
+              return cb(null, resutlArray);
+            });
+          },
+          (arg1, cb) => {
+            let resutlArray = arg1;
+            async.mapLimit(splitArray[2], 10, (link, callback) => {
+              crawl(link, searchWord, baseUrl, (error, newPages) => {
+                if(error) {
+                  return callback(null, []);
+                } else {
+                  return callback(null, newPages);
+                }
+              });
+            }, (err, resp) => {
+              if(err) { return console.log(err); }
+              for (var t = 0; t < resp.length; ++t) {
+                for (var l = 0; l < resp[t].length; ++l) {
+                  if(resutlArray.indexOf(resp[t][l]) == -1) {
+                    resutlArray.push(resp[t][l]);
+                  }
+                }
+              }
+              return cb(null, resutlArray);
+            });
+          },
+          (arg1, cb) => {
+            let resutlArray = arg1;
+            async.mapLimit(splitArray[3], 10, (link, callback) => {
+              crawl(link, searchWord, baseUrl, (error, newPages) => {
+                if(error) {
+                  return callback(null, []);
+                } else {
+                  return callback(null, newPages);
+                }
+              });
+            }, (err, resp) => {
+              if(err) { return console.log(err); }
+              for (var t = 0; t < resp.length; ++t) {
+                for (var l = 0; l < resp[t].length; ++l) {
+                  if(resutlArray.indexOf(resp[t][l]) == -1) {
+                    resutlArray.push(resp[t][l]);
+                  }
+                }
+              }
+              return cb(null, resutlArray);
+            });
+          },
+          (arg1, cb) => {
+            let resutlArray = arg1;
+            async.mapLimit(splitArray[4], 10, (link, callback) => {
+              crawl(link, searchWord, baseUrl, (error, newPages) => {
+                if(error) {
+                  return callback(null, []);
+                } else {
+                  return callback(null, newPages);
+                }
+              });
+            }, (err, resp) => {
+              if(err) { return console.log(err); }
+              for (var t = 0; t < resp.length; ++t) {
+                for (var l = 0; l < resp[t].length; ++l) {
+                  if(resutlArray.indexOf(resp[t][l]) == -1) {
+                    resutlArray.push(resp[t][l]);
+                  }
+                }
+              }
+              return cb(null, resutlArray);
+            });
+          }
+        ], (err, result) => {
           if(err) { return console.log(err); }
-          console.log(response);
+          console.log(result.length);
         });
-
       }
 
     });
@@ -168,8 +278,6 @@ class CrawlerController {
     store.flushdb((err, response) => {
       console.log(response);
     });
-
-    // console.log(pagesToVist.length);
 
     return res.status(200).json('Hello World');
   }
